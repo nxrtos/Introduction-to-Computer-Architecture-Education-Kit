1adc9127eff7e71656d192e2ea61ecd78f9849f5
Blue Now <bluenow1896@gmail.com>
2023-02-20 17:14:27 -0500

    generate pdfs for each module, and update links in html

--- Computer-Architecture-Lectures-Armv8-A/html/js/config.js
+++ Computer-Architecture-Lectures-Armv8-A/html/js/config.js
@@ -9,10 +9,6 @@ var index =
     {
       "name": "Lecture Presentation",
       "column": "modules"
-	  
-	 
-							  
-						   
     }
   ],
   "modules": [
@@ -20,79 +16,133 @@ var index =
       {
         "column": "modules",
         "name": "Module 1: An Introduction to Computer Architecture",
-        "link": "contents/Lectures/Module_01_An_Introduction_to_Computer_Architecture/Module_1_An_introduction_to_computer_architecture.pptx"
-      }
+        "link": "contents/Lectures/Module_01_An_Introduction_to_Computer_Architecture/Module_1_An_introduction_to_computer_architecture.pdf"
+      },
+        {
+          "column": "modules",
+          "name": "src pptx",
+          "link": "contents/Lectures/Module_01_An_Introduction_to_Computer_Architecture/Module_1_An_introduction_to_computer_architecture.pptx"
+        }
     ],
     [
       {
         "column": "modules",
         "name": "Module 2: Fundamentals of Computer Design",
-        "link": "contents/Lectures/Module_02_Fundamentals_of_Computer_Design/Module_2_Fundamentals_of_computer_design.pptx"
-      }
+        "link": "contents/Lectures/Module_02_Fundamentals_of_Computer_Design/Module_2_Fundamentals_of_computer_design.pdf"
+      },
+        {
+          "column": "modules",
+          "name": "src pptx",
+          "link": "contents/Lectures/Module_02_Fundamentals_of_Computer_Design/Module_2_Fundamentals_of_computer_design.pptx"
+        }
     ],
     [
       {
         "column": "modules",
         "name": "Module 3: Pipelining",
-        "link": "contents/Lectures/Module_03_Pipelining/Module_3_Pipelining.pptx"
-      }
-	  
+        "link": "contents/Lectures/Module_03_Pipelining/Module_3_Pipelining.pdf"
+      },
+        {
+          "column": "modules",
+          "name": "src pptx",
+          "link": "contents/Lectures/Module_03_Pipelining/Module_3_Pipelining.pptx"
+        }
     ],
     [
       {
         "column": "modules",
         "name": "Module 4: Branches and the Limits to Pipelining",
-        "link": "contents/Lectures/Module_04_Branches_and_Limits_to_Pipelining/Module_4_Branches_and_the_limits_to_pipelining.pptx"
-      }
+        "link": "contents/Lectures/Module_04_Branches_and_Limits_to_Pipelining/Module_4_Branches_and_the_limits_to_pipelining.pdf"
+      },
+        {
+          "column": "modules",
+          "name": "src pptx",
+          "link": "contents/Lectures/Module_04_Branches_and_Limits_to_Pipelining/Module_4_Branches_and_the_limits_to_pipelining.pptx"
+        }
     ],
     [
       {
         "column": "modules",
         "name": "Module 5: Exploiting Instruction-Level Parallelism",
-        "link": "contents/Lectures/Module_05_Exploiting_Instruction-Level_Parallelism/Module_5_Exploiting_instruction-level_parallelism.pptx"
-      }
+        "link": "contents/Lectures/Module_05_Exploiting_Instruction-Level_Parallelism/Module_5_Exploiting_instruction-level_parallelism.pdf"
+      },
+        {
+          "column": "modules",
+          "name": "src pptx",
+          "link": "contents/Lectures/Module_05_Exploiting_Instruction-Level_Parallelism/Module_5_Exploiting_instruction-level_parallelism.pptx"
+        }
     ],
     [
-	  {
+	    {
         "column": "modules",
         "name": "Module 6: Memory",
-        "link": "contents/Lectures/Module_06_Memory/Module_6_Memory.pptx"
-      }
+        "link": "contents/Lectures/Module_06_Memory/Module_6_Memory.pdf"
+      },
+        {
+          "column": "modules",
+          "name": "src pptx",
+          "link": "contents/Lectures/Module_06_Memory/Module_6_Memory.pptx"
+        }
     ],
     [
       {
         "column": "modules",
         "name": "Module 7: Caches",
-        "link": "contents/Lectures/Module_07_Caches/Module_7_Caches.pptx"
-      }
+        "link": "contents/Lectures/Module_07_Caches/Module_7_Caches.pdf"
+      },
+        {
+          "column": "modules",
+          "name": "src pptx",
+          "link": "contents/Lectures/Module_07_Caches/Module_7_Caches.pptx"
+        }
     ],
     [
       {
         "column": "modules",
         "name": "Module 8: Multicore",
-        "link": "contents/Lectures/Module_08_Multicore/Module_8_Multicore.pptx"
-      }
+        "link": "contents/Lectures/Module_08_Multicore/Module_8_Multicore.pdf"
+      },
+        {
+          "column": "modules",
+          "name": "src pptx",
+          "link": "contents/Lectures/Module_08_Multicore/Module_8_Multicore.pptx"
+        }
     ],
     [
       {
         "column": "modules",
         "name": "Module 9: Multithreading",
-        "link": "contents/Lectures/Module_09_Multithreading/Module_9_Mulithreading.pptx"
-      }
+        "link": "contents/Lectures/Module_09_Multithreading/Module_9_Mulithreading.pdf"
+      },
+        {
+          "column": "modules",
+          "name": "src pptx",
+          "link": "contents/Lectures/Module_09_Multithreading/Module_9_Mulithreading.pptx"
+        }
     ],
     [
-	  {
+	    {
         "column": "modules",
         "name": "Module 10: Vector, SIMD, GPUs",
-        "link": "contents/Lectures/Module_10_Vector_SIMD_GPUs/Module_10_Vector__SIMD__GPUs.pptx"
-      }
+        "link": "contents/Lectures/Module_10_Vector_SIMD_GPUs/Module_10_Vector__SIMD__GPUs.pdf"
+      },
+        {
+          "column": "modules",
+          "name": "src pptx",
+          "link": "contents/Lectures/Module_10_Vector_SIMD_GPUs/Module_10_Vector__SIMD__GPUs.pptx"
+        }
     ],
     [
       {
         "column": "modules",
         "name": "Module 11: SoC Case Study",
-        "link": "contents/Lectures/Module_11_SoC_Case_Study/Module_11_SoC_Study.pptx"
-      }
+        "link": "contents/Lectures/Module_11_SoC_Case_Study/Module_11_SoC_Study.pdf"
+      },
+        {
+          "column": "modules",
+          "name": "src pptx",
+          "link": "contents/Lectures/Module_11_SoC_Case_Study/Module_11_SoC_Study.pptx"
+        }
     ],
   ]
 
